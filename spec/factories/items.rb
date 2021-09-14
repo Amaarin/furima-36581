4186fac25fc66c638e184479439b5e91c36061a3
Amaarin <88422638+Amaarin@users.noreply.github.com>
2021-09-14 11:42:05 +0900

    クォーテーションの削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name           {'商品名'}
     description         {'説明'}
-    category_id         {'2'}
-    status_id           {'2'}
-    shipping_cost_id    {'2'}
-    prefecture_id       {'2'}
-    scheduled_day_id    {'2'}
-    price               {'300'}
+    category_id         {2}
+    status_id           {2}
+    shipping_cost_id    {2}
+    prefecture_id       {2}
+    scheduled_day_id    {2}
+    price               {300}
 
     association :user
 
