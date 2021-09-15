08a0b561271bc0373ab07ebdce226969b0c764bf
Amaarin <88422638+Amaarin@users.noreply.github.com>
2021-09-15 16:36:26 +0900

    表示の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,9 +17,11 @@ class Item < ApplicationRecord
 
 
   belongs_to :user
+  has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_cost
-  has_one_attached :image
+  
  
   
   def was_attached?
