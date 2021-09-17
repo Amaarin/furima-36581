ba4179e7247d25fa14afbbedd92ff335d84f9722
Amaarin <88422638+Amaarin@users.noreply.github.com>
2021-09-17 21:44:29 +0900

    商品購入機能修正

--- app/models/buy_shipping_address.rb
+++ app/models/buy_shipping_address.rb
@@ -15,7 +15,7 @@ class BuyShippingAddress
 
   def save
     buy = Buy.create(item_id: item_id, user_id: user_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, donation_id: donation.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building_name: building_name)
   end
  
 
