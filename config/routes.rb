29a7a65290f19c288d8a918ffaf2f4f62407e9d3
Amaarin <88422638+Amaarin@users.noreply.github.com>
2021-09-18 16:59:43 +0900

    デバッグコード、アクション削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
     resources :items do
-    resources :buys, only: [:index, :new, :create] 
+    resources :buys, only: [:index, :create] 
   end
 end
